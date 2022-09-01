2db9f51a7aaff72a06b7a8fc90bc03b4f2324c21
Rab-son <rabsonsayendajnr@gmail.com>
2022-09-01 21:52:46 +0200

    first file

---
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    if matrix is not None:
+        new = []
+        for num in matrix:
+            new.append(list(map(lambda x: x*x, num)))
+        return new
