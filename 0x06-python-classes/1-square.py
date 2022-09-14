2a0361a481e82a5f3fa140b2ee9c6d2035a314d8
Rab-son <rabsonsayendajnr@gmail.com>
2022-09-14 05:17:11 +0200

    square 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,10 +1,19 @@
 #!/usr/bin/python3
-class Square:
-    """Represents a square.
-    Private instance attribute: size.
-    Instantiation with size (no type/value verification).
-    """
+"""Square module definition.
 
+This module defines a simple `Square` class
+"""
+
+
+class Square:
+    """A simple ``Square`` class
+
+    Attributes:
+        size (`int`): The size of the ``Square``.
+    """
     def __init__(self, size):
-        """Initializes the data."""
-        self.__size = size
+        """Constructs a ``Square`` objet
+        Args:
+            size (`int`): The size of the ``Square``.
+        """
+        self._Square__size = size
