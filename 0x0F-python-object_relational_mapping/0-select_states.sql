b0fa0bcc55e81221b6b9f5de53e7ab5c56b96f58
Rab-son <rabsonsayendajnr@gmail.com>
2022-12-08 22:22:37 +0200

    removing project files

--- 0x0F-python-object_relational_mapping/0-select_states.sql
+++
@@ -1,9 +0,0 @@
--- Create states table in hbtn_0e_0_usa with some data
-CREATE DATABASE IF NOT EXISTS hbtn_0e_0_usa;
-USE hbtn_0e_0_usa;
-CREATE TABLE IF NOT EXISTS states ( 
-	id INT NOT NULL AUTO_INCREMENT, 
-	name VARCHAR(256) NOT NULL,
-	PRIMARY KEY (id)
-);
-INSERT INTO states (name) VALUES (1, 'California'), (2, 'Arizona'), (3, 'Texas'), (4, 'New York'), (5, 'Nevada');
